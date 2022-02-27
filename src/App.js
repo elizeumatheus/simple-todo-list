3729652cb94ab8807307f96480194fe1d38572f9
Elizeu Louzeiro <emjlouzeiro@gmail.com>
2022-02-27 00:44:28 -0300

    criação da funcionalidade de lista

--- src/App.js
+++ src/App.js
@@ -1,19 +1,73 @@
 import React, { Component } from "react";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
+import "./App.css";
+import Item from "./components/Item/Item";
 
 class App extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      list: [],
+      value: "",
+    };
   }
 
+  onRemoveItem = (item) => {
+    const { list } = this.state;
+
+    const newList = list.filter((el) => el !== item);
+
+    this.setState((estadoAtual) => ({ ...estadoAtual, list: newList }));
+  };
+
+  onChangeInputValue = (event) => {
+    const { value } = event.target;
+
+    this.setState((estadoAtual) => ({ ...estadoAtual, value }));
+  };
+
+  onSubmitForm = (event) => {
+    event.preventDefault();
+
+    const { list, value } = this.state;
+
+    const existsItemOnList = list.some((el) => el === value);
+
+    if (existsItemOnList) {
+      alert("Item already exists");
+      this.setState((estadoAtual) => ({ ...estadoAtual, value: "" }));
+      return;
+    }
+
+    this.setState((estadoAtual) => ({
+      value: "",
+      list: [...estadoAtual.list, estadoAtual.value],
+    }));
+  };
+
   render() {
+    const { list, value } = this.state;
+
     return (
       <main>
-        <Input placeholder="Enter item" />
-        <Button>Submit</Button>
+        <section id="main-section">
+          <h1 id="list-title">You have {list.length} TODO's</h1>
+          <div>
+            {list.map((item, index) => (
+              <Item key={index} item={item} onRemoveItem={this.onRemoveItem} />
+            ))}
+          </div>
+          <form onSubmit={this.onSubmitForm} className="form">
+            <Input
+              placeholder="Enter item"
+              value={value}
+              onChange={this.onChangeInputValue}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </section>
       </main>
     );
   }
