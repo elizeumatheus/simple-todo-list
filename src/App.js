888014bb0bca5b0c598bd4b37bd7d35763f609fe
Elizeu Louzeiro <emjlouzeiro@gmail.com>
2022-02-26 23:38:03 -0300

    adição dos components input e button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
   render() {
-    return <main></main>;
+    return (
+      <main>
+        <Input placeholder="Enter item" />
+        <Button>Submit</Button>
+      </main>
+    );
   }
 }
 
